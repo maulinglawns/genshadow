5a0b63ffead4ebc4c89a7f5d6354e37eed70cf0c
Magnus Wallin <magnuswallin@tutanota.com>
2018-03-26 20:14:59 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # genshadow
 Simple command line utility for generating sha512 hash
+
+```
+genshadow 0.1.2
+Magnus Wallin <magnuswallin@tutanota.com>
+A simple compiled command line utility for generating a sha512 hash (for use in /etc/shadow).
+Exit codes:
+0: Success
+Non-zero: Failure
+
+USAGE:
+    genshadow
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+```
+
+Example:
+```
+./genshadow 
+Enter your password: 
+Please verify your password: 
+Your sha512 hash:
+$6$alnsK3Kmj2FWJ/h2$uc/v7Fgmv.kd6aWimDG7b7vL/UnOe8TnKETI4wdqJAb/uRL7jXaKIgwtVcOEwzEgGeJrYKHOouSiAyCsPNzdl/
+```
---
+++ src/main.rs
@@ -0,0 +1,60 @@
+extern crate rpassword;
+extern crate pwhash;
+extern crate clap;
+use pwhash::sha512_crypt;
+use clap::App;
+
+fn getpass() -> String {
+    // Ask for password and verify
+    // Return values:
+    // OK: The password as string
+    loop {
+        let pass1 = rpassword::prompt_password_stdout("Enter your password: ")
+            .unwrap();
+        if pass1.len() < 6 {
+            println!("Password is too short, use at least 6 characters.");
+            continue;
+        }
+        let pass2 = rpassword::prompt_password_stdout("Please verify your password: ")
+            .unwrap();
+    
+        if pass1 != pass2 {
+            println!("Passwords doesn't match. Try again.");
+            continue;
+        }
+        return pass1
+    }
+}
+
+fn make_sha512_hash(pass_text: &str) -> bool {
+    // Create sha512 hash
+    // Arguments:
+    // A string
+    // Return values:
+    // OK: true
+    // Err: false
+    if pass_text.len() < 6 {
+        println!("Blatantly refusing to generate hash. \
+        Password is too short!");
+        return false;
+    }
+    println!("Your sha512 hash:\n{}", sha512_crypt::hash(pass_text).unwrap());
+    return true;
+}
+
+fn main() {
+    // Parse arguments
+    App::new("genshadow")
+        .version("0.1.2")
+        .about("A simple compiled command line utility for generating \
+        a sha512 hash (for use in /etc/shadow).\n\
+        Exit codes:\n\
+        0: Success\n\
+        Non-zero: Failure")
+        .author("Magnus Wallin <magnuswallin@tutanota.com>")
+        .get_matches();
+    // Ask for password
+    let newpass = getpass();
+    // Generate hash & print to stdout
+    make_sha512_hash(&newpass);
+}
